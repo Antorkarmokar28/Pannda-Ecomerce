8f8c3178c1ab0b3182e4103c6ae037fb16afc97f
Antorkarmokar28 <antorkarmokar28@gmail.com>
2022-09-22 00:40:02 +0600

    js somthing code changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
-
 const background = document.getElementById('backpacks');
 background.style.backgroundColor = 'tomato';
 
 const bgchanges = document.getElementById('bgChange');
 bgchanges.addEventListener('dblclick', function(){
     document.getElementById('bgChange').style.backgroundColor = 'tomato';
+});
+
+document.getElementById('input-field').addEventListener('keyup', function(event){
+    const text = event.target.value;
+    const button = document.getElementById('submit-btn');
+    if(text == 'email'){
+        button.removeAttribute('disabled');
+    }
+    else{
+        button.setAttribute('disabled', true);
+    }
 })
