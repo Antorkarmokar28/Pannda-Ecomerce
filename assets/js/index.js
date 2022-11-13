9cac812a65d1d1b60c5dc15750c47407f1575de1
Antorkarmokar28 <antorkarmokar28@gmail.com>
2022-11-13 23:43:52 +0600

    something js code added and something code modify

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,25 @@
 const background = document.getElementById('backpacks');
 background.style.backgroundColor = 'tomato';
 
-const bgchanges = document.getElementById('bgChange');
-bgchanges.addEventListener('dblclick', function(){
+const bgChange = document.getElementById('bgChange');
+bgChange.addEventListener('dblclick', function(){
     document.getElementById('bgChange').style.backgroundColor = 'tomato';
-});
+})
 
 document.getElementById('input-field').addEventListener('keyup', function(event){
     const text = event.target.value;
     const button = document.getElementById('submit-btn');
-    if(text == 'email'){
+    if(text === 'email'){
         button.removeAttribute('disabled');
     }
     else{
         button.setAttribute('disabled', true);
     }
 })
+
+const productRemoves = document.getElementsByClassName('add-btn');
+for(const productRemove of productRemoves){
+    productRemove.addEventListener('click', function(event){
+        event.target.parentNode.removeChild(event.target);
+    })
+}
